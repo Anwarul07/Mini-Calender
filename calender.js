c5745aaa65002025b9963c4e21bfd2ac96a27e2d
Anwarul07 <mdanwarul064@gmail.com>
2024-05-01 23:26:20 +0530

    Update Calender.js

--- calender.js
+++ calender.js
@@ -1,1 +1,16 @@
-console.log("See the date easily here")
+console.log("See the date easily here");
+
+let date = document.getElementById("date");
+let mon = document.getElementById("mon");
+let day = document.getElementById("day");
+let yera = document.getElementById("year");
+
+let week = ["Sunday", "Monday", "Tuesday", " Wednessday ", "Thursday ", "Friday", " Saturday "]
+let month = ["January", " Febuary", " March", " April", " May ", "June ", "July", " August", " September", " October", "November", "December"]
+
+setTimeout(() => {
+    date.innerHTML = (new Date().getDate()<10 ?"0": "")+ new Date().getDate();
+    mon.innerHTML = month[new Date().getMonth()];
+    year.innerHTML = new Date().getFullYear()
+    day.innerHTML = week[new Date().getDay()]
+}, 500);
